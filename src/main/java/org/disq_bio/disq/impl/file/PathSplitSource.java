9837ac23d984ebc673adb40b9149f94fdb48dcf1
Tom White <tom.e.white@gmail.com>
2018-09-25 15:47:22 +0100

    [DISQ-33] Duplicate reads bug (#34)
    * Expose bug where in some cases (when a BGZF block boundary coincides with a split partition)
    reads are included in two neighboring partitions.
    
    * Fix bug producing duplicate reads when a BGZF block boundary coincides with a split partition.
    
    * Add debug logging for path splits and path chunks.

--- src/main/java/org/disq_bio/disq/impl/file/PathSplitSource.java
+++ src/main/java/org/disq_bio/disq/impl/file/PathSplitSource.java
@@ -3,6 +3,7 @@ package org.disq_bio.disq.impl.file;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
@@ -11,12 +12,15 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import scala.Tuple2;
 
 public class PathSplitSource implements Serializable {
 
   private static final int DEFAULT_NIO_SPLIT_SIZE = 128 * 1024 * 1024;
+  private static final Logger logger = LoggerFactory.getLogger(PathSplitSource.class);
 
   private final FileSystemWrapper fileSystemWrapper;
 
@@ -48,12 +52,14 @@ public class PathSplitSource implements Serializable {
 
       List<Long> range = LongStream.range(0, numSplits).boxed().collect(Collectors.toList());
       return jsc.parallelize(range, numSplits)
-          .map(idx -> idx * actualSplitSize)
-          .flatMap(
-              splitStart -> {
+          .map(
+              idx -> {
+                long splitStart = idx * actualSplitSize;
                 final long splitEnd =
                     splitStart + actualSplitSize > len ? len : splitStart + actualSplitSize;
-                return Collections.singleton(new PathSplit(path, splitStart, splitEnd)).iterator();
+                PathSplit pathSplit = new PathSplit(path, splitStart, splitEnd);
+                logger.debug("PathSplit for partition {}: {}", idx, pathSplit);
+                return pathSplit;
               });
     } else {
       // Use Hadoop FileSystem API to maintain file locality by using Hadoop's FileInputFormat
@@ -64,17 +70,20 @@ public class PathSplitSource implements Serializable {
       }
       return jsc.newAPIHadoopFile(
               path, FileSplitInputFormat.class, Void.class, FileSplit.class, conf)
-          .flatMap(
-              (FlatMapFunction<Tuple2<Void, FileSplit>, PathSplit>)
-                  t2 -> {
+          .mapPartitionsWithIndex(
+              (Function2<Integer, Iterator<Tuple2<Void, FileSplit>>, Iterator<PathSplit>>)
+                  (idx, it) -> {
+                    Tuple2<Void, FileSplit> t2 = it.next(); // one file split per partition
                     FileSplit fileSplit = t2._2();
                     PathSplit pathSplit =
                         new PathSplit(
                             fileSplit.getPath().toString(),
                             fileSplit.getStart(),
                             fileSplit.getStart() + fileSplit.getLength());
+                    logger.debug("PathSplit for partition {}: {}", idx, pathSplit);
                     return Collections.singleton(pathSplit).iterator();
-                  });
+                  },
+              true);
     }
   }
 }
