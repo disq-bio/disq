f92a6747e72f8fedd62e5af6a1f4b2688a714221
Tom White <tom.e.white@gmail.com>
2019-01-15 10:00:52 +0000

    [DISQ-74] Update copyright years to include 2019
    Fixes #74.

--- src/test/java/org/disq_bio/disq/BcftoolsTestUtil.java
+++ src/test/java/org/disq_bio/disq/BcftoolsTestUtil.java
@@ -3,7 +3,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2018 Disq contributors
+ * Copyright (c) 2018-2019 Disq contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
