b629f8a593f5901347ddd8f264d1790efe197e75
Tom White <tom.e.white@gmail.com>
2019-01-23 10:35:40 +0000

    Fix bcftools installation command

--- src/build/bin/install-bcftools.sh
+++ src/build/bin/install-bcftools.sh
@@ -2,4 +2,4 @@
 set -ex
 wget https://github.com/samtools/bcftools/releases/download/1.3/bcftools-1.3.tar.bz2
 tar -xjvf bcftools-1.3.tar.bz2
-cd bcftools-1.3 && ./configure --prefix=/usr && make && sudo make install
+cd bcftools-1.3 && make && sudo make prefix=/usr install
