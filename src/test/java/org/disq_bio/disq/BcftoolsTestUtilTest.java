757d1a39e1442ea9f050520dc3aa1ded0ea10eae
Louis Bergelson <louisb@broadinstitute.org>
2020-04-29 13:11:03 -0400

    Fixing RealWorldFilesIT (#132) (#136)
    * Fixing RealWorldFilesIT (#132)
    
    * Fixes https://github.com/disq-bio/disq/issues/132
    * There was a bug in BcftoolsTestUtil which counted empty vcfs as having 1 record.
      An empty vcf was added to the GATK test files which we test againts which then caused our tests to fail.
      This fixes the problem by correctly counting empty vcfs as having 0 records when testing with Bcftools.
    
    * add forgotten license
    
    * fix formatting

---
+++ src/test/java/org/disq_bio/disq/BcftoolsTestUtilTest.java
@@ -0,0 +1,54 @@
+/*
+ * Disq
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018-2019 Disq contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.disq_bio.disq;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitParamsRunner.class)
+public class BcftoolsTestUtilTest extends BaseTest {
+
+  private Object[] parametersForTestBcfToolsCountVariants() {
+    return new Object[][] {
+      {"test.vcf", 5},
+      {"testEmpty.vcf", 0}
+    };
+  }
+
+  @Test
+  @Parameters
+  public void testBcfToolsCountVariants(String vcfPath, int expected)
+      throws IOException, URISyntaxException {
+    Assume.assumeTrue(BcftoolsTestUtil.isBcftoolsAvailable());
+    Assert.assertEquals(expected, BcftoolsTestUtil.countVariants(getPath(vcfPath)));
+  }
+}
