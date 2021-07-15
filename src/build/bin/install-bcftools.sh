7bf157b570647b1ab5b45b3414bd87584fd78fb5
Michael L Heuer <heuermh@acm.org>
2021-07-15 13:18:25 -0500

    Update samtools and bcftools versions to 1.13.

--- src/build/bin/install-bcftools.sh
+++ src/build/bin/install-bcftools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/samtools/bcftools/releases/download/1.3/bcftools-1.3.tar.bz2
-tar -xjvf bcftools-1.3.tar.bz2
-cd bcftools-1.3 && make && sudo make prefix=/usr install
+wget https://github.com/samtools/bcftools/releases/download/1.13/bcftools-1.13.tar.bz2
+tar -xjvf bcftools-1.13.tar.bz2
+cd bcftools-1.13 && make && sudo make prefix=/usr install
