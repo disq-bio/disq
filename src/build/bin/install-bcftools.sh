19ee4053ebc6f4923a7215001707235db3e18b23
Chris Norman <cnorman@broadinstitute.org>
2022-01-31 14:36:30 -0500

    Update to samtools and bcftools 1.14.

--- src/build/bin/install-bcftools.sh
+++ src/build/bin/install-bcftools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/samtools/bcftools/releases/download/1.13/bcftools-1.13.tar.bz2
-tar -xjvf bcftools-1.13.tar.bz2
-cd bcftools-1.13 && make && sudo make prefix=/usr install
+wget https://github.com/samtools/bcftools/releases/download/1.14/bcftools-1.14.tar.bz2
+tar -xjvf bcftools-1.14.tar.bz2
+cd bcftools-1.14 && make && sudo make prefix=/usr install
--- src/build/bin/install-samtools.sh
+++ src/build/bin/install-samtools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/samtools/samtools/releases/download/1.13/samtools-1.13.tar.bz2
-tar -xjvf samtools-1.13.tar.bz2
-cd samtools-1.13 && ./configure --prefix=/usr && make && sudo make install
+wget https://github.com/samtools/samtools/releases/download/1.14/samtools-1.14.tar.bz2
+tar -xjvf samtools-1.14.tar.bz2
+cd samtools-1.14 && ./configure --prefix=/usr && make && sudo make install
